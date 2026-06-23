0178d60c670bb5b719a3093b7770154fc2013a16
leo-Ito007 <leonardo.salles.felipe@escola.pr.gov.br>
2026-06-23 12:32:05 -0300

    Add files via upload

---
+++ src/componentes/index.js
@@ -0,0 +1,29 @@
+import image from './panqueca.png';
+function Cardapio() {
+        return (
+            <div>
+ <h1>Cardápio</h1>
+ <h2>Pratos</h2>
+ <ul>
+   <li>Feijoada - R$ 25,00</li>
+   <li>Panqueca - R$ 15,00</li>
+   <li>Churrasco - R$ 30,00</li>
+ </ul>
+ <h2>Bebidas</h2>
+ <ul>
+   <li>Refrigerante - R$ 5,00</li>
+   <li>Soda - R$ 6,00</li>
+   <li>Suco de frutas - R$ 8,00</li>
+ </ul>     
+ <h2> Sobremesas</h2>
+ <ul>
+   <li>Brigadeiro - R$ 3,00</li>
+   <li>Pudim - R$ 4,00</li>
+   <li>Frutas - R$ 2,00</li>
+ </ul>  
+</div>
+        );
+
+}
+
+export default Cardapio;
