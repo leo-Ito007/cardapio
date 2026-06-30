7947b71366e730b1f54ba0c9b1cb94c65363ecf7
leo-Ito007 <leonardo.salles.felipe@escola.pr.gov.br>
2026-06-30 11:45:58 -0300

    Add files via upload

--- src/componentes/index.js
+++ src/componentes/index.js
@@ -1,25 +1,45 @@
-import image from './panqueca.png';
+
+import feijuada from './feijuada.png';
+import panqueca from './panqueca.png';
+import churrasco from './churrasco.png';
+import refrigerante from './refrigerante.png';
+import soda from './soda.png';
+import suco from './suco.png';
+import brigadeiro from './brigadeiro.png';
+import pudim from './pudim.png';
+import fruta from './fruta.png';
+import './cardapio.css';
+
 function Cardapio() {
         return (
             <div>
  <h1>Cardápio</h1>
  <h2>Pratos</h2>
  <ul>
-   <li>Feijoada - R$ 25,00</li>
-   <li>Panqueca - R$ 15,00</li>
-   <li>Churrasco - R$ 30,00</li>
+   <li>Feijoada - R$ 25,00
+   <img src={feijuada} alt="feijuada" /></li>
+   <li>Panqueca - R$ 15,00
+   <img src={panqueca} alt="panqueca" /></li>
+   <li>Churrasco - R$ 30,00
+   <img src={churrasco} alt="churrasco" /></li>
  </ul>
  <h2>Bebidas</h2>
  <ul>
-   <li>Refrigerante - R$ 5,00</li>
-   <li>Soda - R$ 6,00</li>
-   <li>Suco de frutas - R$ 8,00</li>
+   <li>Refrigerante - R$ 5,00
+   <img src={refrigerante} alt="refrigerante" /></li>
+   <li>Soda - R$ 6,00
+   <img src={soda} alt="soda" /></li>
+   <li>Suco de frutas - R$ 8,00
+   <img src={suco} alt="suco" /></li>
  </ul>     
  <h2> Sobremesas</h2>
  <ul>
-   <li>Brigadeiro - R$ 3,00</li>
-   <li>Pudim - R$ 4,00</li>
-   <li>Frutas - R$ 2,00</li>
+   <li>Brigadeiro - R$ 3,00
+   <img src={brigadeiro} alt="brigadeiro" /></li>
+   <li>Pudim - R$ 4,00
+   <img src={pudim} alt="pudim" /></li>
+   <li>Frutas - R$ 2,00
+   <img src={fruta} alt="fruta" /></li>
  </ul>  
 </div>
         );
